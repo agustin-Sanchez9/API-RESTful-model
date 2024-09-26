d79503571a8f7c8a6af7897fbffbf4d61fe451ce
agustin-Sanchez9 <agustinsanchezp123@gmail.com>
2024-09-25 21:25:20 -0300

    update

---
+++ ARm/src/main/java/com/example/ARm/entity/StudentEntity.java
@@ -0,0 +1,55 @@
+package com.example.ARm.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class StudentEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "birth_date")
+	private Date birth_date;
+
+	
+	// Getters and Setters
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirth_date() {
+		return birth_date;
+	}
+
+	public void setBirth_date(Date birth_date) {
+		this.birth_date = birth_date;
+	}
+	
+	
+	
+}
---
+++ ARm/src/main/java/com/example/ARm/entity/TopicEntity.java
@@ -0,0 +1,5 @@
+package com.example.ARm.entity;
+
+public class TopicEntity {
+
+}
